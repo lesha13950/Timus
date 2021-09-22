51870160c863c52be4a738a239decd6965a89fe4
lesha13950 <kurepin1985@mail.ru>
2021-09-22 19:55:14 +0400

    Task 1020 solution

---
+++ Timus/t1020/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens;
+            int count, i = 1;
+            double radius, x0, y0, x1, y1, x2, y2, l = 0;
+
+            tokens = Console.ReadLine().Split();
+            count = int.Parse(tokens[0]);
+            radius = double.Parse(tokens[1]);
+
+            tokens = Console.ReadLine().Split();
+            x0 = x1 = double.Parse(tokens[0]);
+            y0 = y1 = double.Parse(tokens[1]);
+            while (i < count)
+            {
+                tokens = Console.ReadLine().Split();
+                x2 = double.Parse(tokens[0]);
+                y2 = double.Parse(tokens[1]);
+                l += Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                x1 = x2;
+                y1 = y2;
+                i++;
+            }
+            l += Math.Sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+            l += 2 * Math.PI * radius;
+            Console.WriteLine($"{l:f2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Timus/t1020/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("t1020")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("t1020")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9d3ea115-0ca4-4555-849a-5356b4210a3c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
