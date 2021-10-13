90809ba67c60aaa4979c31c1ae2ae129551137d1
lesha13950 <kurepin1985@mail.ru>
2021-10-13 15:12:01 +0400

    Task 1082 solution

---
+++ Timus/t1082/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1082
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(i);
+                if (i != N - 1) Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
