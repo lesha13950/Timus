8e893de424292db5306b1cf506074c85b5bf90e9
lesha13950 <kurepin1985@mail.ru>
2021-10-13 15:58:10 +0400

    Task 1180 solution

---
+++ Timus/t1180/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace t1180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger N = BigInteger.Parse(Console.ReadLine().Trim());
+            BigInteger r = N % 3;
+
+            if (r == 0)
+            {
+                Console.WriteLine("2");
+            }
+            else
+            {
+                Console.WriteLine("1");
+                Console.WriteLine(r);
+            }
+            Console.ReadLine();
+        }
+    }
+}
