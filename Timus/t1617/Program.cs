a981a223a35ac155649a9f7bb9ea0eb125cf23d6
lesha13950 <kurepin1985@mail.ru>
2021-11-18 23:05:34 +0400

    Task 1617 solution

---
+++ Timus/t1617/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1617
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            int[] d = new int[101];
+            int v = 0;
+            int i;
+
+            for (i = 0; i < N; i++)
+            {
+                int t = int.Parse(Console.ReadLine().Trim());
+                d[t - 600]++;
+            }
+
+            for (i = 0; i <= 100; i++) v += d[i] / 4;
+
+            Console.WriteLine(v);
+            Console.ReadLine();
+        }
+    }
+}
