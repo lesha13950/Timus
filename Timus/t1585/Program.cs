8c26c642fd9a9f46ead962589cbab7f7ef7105b0
lesha13950 <kurepin1985@mail.ru>
2021-11-18 22:55:22 +0400

    Task 1585 solution

---
+++ Timus/t1585/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1585
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
