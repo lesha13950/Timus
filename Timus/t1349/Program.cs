c6a42d0c352dbcd8b00e8159ad4fcc4eb9c8b68b
lesha13950 <kurepin1985@mail.ru>
2021-11-18 15:43:38 +0400

    Task 1349 solution

---
+++ Timus/t1349/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1349
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a, b, c;
+
+            if (n == 1) Console.WriteLine("1 2 3");
+            else if (n == 2) Console.WriteLine("3 4 5");
+            else Console.WriteLine("-1");
+            Console.ReadLine();
+        }
+    }
+}
