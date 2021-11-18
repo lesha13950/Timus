6747851662b470da43bdc6f65b50c4b2b2262d42
lesha13950 <kurepin1985@mail.ru>
2021-11-18 22:46:55 +0400

    Task 1582 solution

---
+++ Timus/t1582/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1582
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            string[] tokens = Console.ReadLine().Trim().Split(' ');
+            double k1 = double.Parse(tokens[0]), k2 = double.Parse(tokens[1]), k3 = double.Parse(tokens[2]);
+
+            Console.WriteLine((int)(0.5 + 1000 * k1 * k2 * k3 / (k1 * k2 + k2 * k3 + k1 * k3)));
+            Console.ReadLine();
+        }
+    }
+}
