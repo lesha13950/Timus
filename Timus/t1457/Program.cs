eb488c67937ff9a70366faa5bd8592149628967f
lesha13950 <kurepin1985@mail.ru>
2021-11-18 20:50:00 +0400

    Task 1457 solution

---
+++ Timus/t1457/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1457
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            string[] tokens = Console.ReadLine().Split(' ');
+            int s = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                s += int.Parse(tokens[i]);
+            }
+
+            Console.WriteLine("{0:f6}", 1.0 * s / N);
+            Console.ReadLine();
+        }
+    }
+}
