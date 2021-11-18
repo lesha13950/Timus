6231667fc2a82395380eec42b7ff5a2205f1343e
lesha13950 <kurepin1985@mail.ru>
2021-11-18 21:21:45 +0400

    Task 1496 solution

---
+++ Timus/t1496/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1496
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            string[] commands = new string[N];
+            string spamer = "";
+            int i;
+
+            for (i = 0; i < N; i++) commands[i] = Console.ReadLine().Trim();
+            Array.Sort(commands);
+
+            for (i = 1; i < N; i++)
+            {
+                if (commands[i] != spamer)
+                {
+                    if (commands[i] == commands[i - 1])
+                    {
+                        spamer = commands[i];
+                        Console.WriteLine(spamer);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
