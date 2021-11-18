ec0cb41da11176c664c9ca3d1d911b35cb1b74e8
lesha13950 <kurepin1985@mail.ru>
2021-11-18 15:29:08 +0400

    Task 1327 solution

---
+++ Timus/t1327/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine().Trim());
+            int B = int.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine(((B - A) + 1) / 2 + (A % 2 & B % 2));
+            Console.ReadLine();
+        }
+    }
+}
