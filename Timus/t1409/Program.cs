ebb178743e93a087907fbd3b9aac12b2d73b2b36
lesha13950 <kurepin1985@mail.ru>
2021-11-18 20:30:31 +0400

    Task 1409 solution

---
+++ Timus/t1409/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            int a = int.Parse(tokens[0]), b = int.Parse(tokens[1]);
+
+            Console.WriteLine("{0} {1}", b - 1, a - 1);
+            Console.ReadLine();
+        }
+    }
+}
