95f48b80dc8e77d5b62ed512f2296cebbc43abe7
lesha13950 <kurepin1985@mail.ru>
2021-11-18 22:55:51 +0400

    Task 1585 - save

--- Timus/t1585/Program.cs
+++ Timus/t1585/Program.cs
@@ -10,6 +10,26 @@ namespace t1585
     {
         static void Main(string[] args)
         {
+            int N = int.Parse(Console.ReadLine().Trim());
+            int e, l, m, max;
+
+            e = l = m = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine().Trim();
+                if (s[0] == 'E') e++;
+                if (s[0] == 'M') m++;
+                if (s[0] == 'L') l++;
+            }
+
+            max = e;
+            if (l > max) max = l;
+            if (m > max) max = m;
+
+            if (max == e) Console.WriteLine("Emperor Penguin");
+            if (max == l) Console.WriteLine("Little Penguin");
+            if (max == m) Console.WriteLine("Macaroni Penguin");
+            Console.ReadLine();
         }
     }
 }
