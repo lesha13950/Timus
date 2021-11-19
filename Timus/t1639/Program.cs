9f50841b01071361b31b311fa491fe063189cf8e
lesha13950 <kurepin1985@mail.ru>
2021-11-19 14:52:28 +0400

    Task 1639 solution

---
+++ Timus/t1639/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1639
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Trim().Split(' ');
+            int m = int.Parse(tokens[0]), n = int.Parse(tokens[1]);
+
+            if ((m * n) % 2 == 0) Console.WriteLine("[:=[first]");
+            else Console.WriteLine("[second]=:]");
+
+            Console.ReadLine();
+        }
+    }
+}
