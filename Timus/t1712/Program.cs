84c3661cac9a6de11a1f014b0693623127e5c7b3
lesha13950 <kurepin1985@mail.ru>
2021-11-19 15:33:51 +0400

    Task 1712 solution

---
+++ Timus/t1712/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1712
+{
+    class Program
+    {
+        static string ApplyGrid(string[] grid, string[] secret)
+        {
+            string result = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (grid[i][j] == 'X') result += secret[i][j];
+                }
+            }
+
+            return result;
+        }
+
+        static void RotateGrid(ref string[] grid)
+        {
+            string[] result = new string[4] { "", "", "", "" };
+
+            for (int j = 0; j < 4; j++)
+                for (int i = 0; i < 4; i++)
+                    result[j] += grid[3 - i][j];
+            grid = result;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] grid = new string[4], secret = new string[4];
+            string password = "";
+            int i;
+
+            for (i = 0; i < 4; i++) grid[i] = Console.ReadLine().Trim();
+            for (i = 0; i < 4; i++) secret[i] = Console.ReadLine().Trim();
+
+            for (i = 0; i < 4; i++)
+            {
+                password += ApplyGrid(grid, secret);
+                RotateGrid(ref grid);
+            }
+            Console.WriteLine(password);
+            Console.ReadLine();
+        }
+    }
+}
