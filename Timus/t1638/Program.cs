250e35cb8664e9957257f4c301a74dcc384f617c
lesha13950 <kurepin1985@mail.ru>
2021-11-19 14:28:22 +0400

    Task 1638 solution

---
+++ Timus/t1638/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1638
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Trim().Split(' ');
+            int wt = int.Parse(tokens[0]);
+            int wp = int.Parse(tokens[1]);
+            int first = int.Parse(tokens[2]);
+            int last = int.Parse(tokens[3]);
+
+            if (last == first) Console.WriteLine(wt);
+            else if (last > first) Console.WriteLine((last - first - 1) * (wt + 2 * wp) + 2 * wp);
+            else Console.WriteLine((first - last + 1) * wt + (first - last) * wp * 2);
+            Console.ReadLine();
+        }
+    }
+}
