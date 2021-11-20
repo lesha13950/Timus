73205b12d78138be8358103eb38198a8353d3ec7
lesha13950 <kurepin1985@mail.ru>
2021-11-20 21:49:52 +0400

    Task 1013 speed-up
    Using matrix fast exponentiation algorithm

--- Timus/t1013/Program.cs
+++ Timus/t1013/Program.cs
@@ -5,31 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
 
-namespace t1013
+namespace t1012
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine().Trim()), k = int.Parse(Console.ReadLine().Trim());
-            int M = int.Parse(Console.ReadLine().Trim());
-            BigInteger F = 0, F3, F4;
+            BigInteger N = BigInteger.Parse(Console.ReadLine().Trim()), k = BigInteger.Parse(Console.ReadLine().Trim());
+            BigInteger M = BigInteger.Parse(Console.ReadLine().Trim());
+            BigInteger F2, F3; // vector F
+            BigInteger F = -1; //result
+            BigInteger a = 0, b = 1, c, d; // matrix
 
-            if (N != 2)
+            c = d = k - 1;
+
+            F2 = k * k - k;
+            F3 = (k - 1) * (k * k - 1);
+
+            if (N == 2) F = F2;
+            else if (N == 3) F = F3;
+            else
             {
-                F3 = (k - 1) * (k * k - 1);
-                F4 = (k - 1) * (k * k - 1) * k - (k - 1) * (k - 1);
-                if (N == 3) F = F3;
-                else if (N == 4) F = F4;
-                else
+                BigInteger p = N - 3;
+                while (p > 0)
                 {
-                    for (int i = 4; i < N; i++)
+                    if ((p & 1) == 1)
                     {
-                        F = (k - 1) * (F3 + F4);
-                        F3 = F4;
-                        F4 = F;
+                        BigInteger tF2 = F2;
+                        F2 = a * F2 + b * F3;
+                        F3 = c * tF2 + d * F3;
                     }
+                    BigInteger ta = a, tb = b, tc = c;
+                    a = a * a + b * c;
+                    b = ta * b + b * d;
+                    c = c * ta + d * c;
+                    d = tc * tb + d * d;
+
+                    p >>= 1;
                 }
+                F = F3;
             }
 
             Console.WriteLine(F % M);
