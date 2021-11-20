8d26b2e1afdb2b8ff9faf32ca47665f9d729e6ee
lesha13950 <kurepin1985@mail.ru>
2021-11-20 16:45:26 +0400

    Task 1014 solution

---
+++ Timus/t1014/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            string q = "";
+
+            if (N == 0) q = "10";
+            else if (N == 1) q = "1";
+            else
+            {
+                for (int i = 9; i >= 2; i--)
+                {
+                    while (N % i == 0)
+                    {
+                        N /= i;
+                        q = i + q;
+                    }
+                }
+                if (N != 1) q = "-1";
+            }
+            
+            Console.WriteLine(q);
+            Console.ReadLine();
+        }
+    }
+}
