dfc3cfeb48f93e160cbadf57f5de3e47cc03d914
lesha13950 <kurepin1985@mail.ru>
2021-11-20 15:21:51 +0400

    Task 1009 solution

---
+++ Timus/t1009/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace t1009
+{
+    class Program
+    {
+        static BigInteger pow(BigInteger n, int k)
+        {
+            BigInteger result = 1;
+            for (int i = 0; i < k; i++) result *= n;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim()), k = int.Parse(Console.ReadLine().Trim());
+            BigInteger[] F = new BigInteger[16];
+
+            // F[N] initialization
+            F[0] = F[1] = 0;
+            F[2] = 1;
+            for (int i = 3; i < N; i++) F[i] = pow(k, i - 2) + (k - 1) * (F[i - 1] + F[i - 2]);
+
+            Console.WriteLine((k - 1) * (pow(k, N - 1) - F[N - 1]));
+            Console.ReadLine();
+        }
+    }
+}
