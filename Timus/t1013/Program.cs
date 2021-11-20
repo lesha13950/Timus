4c9de4c794d5bf1265d11558790590371f8d5886
lesha13950 <kurepin1985@mail.ru>
2021-11-20 18:52:59 +0400

    Task 1013 - initial commit

---
+++ Timus/t1013/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace t1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim()), k = int.Parse(Console.ReadLine().Trim());
+            int M = int.Parse(Console.ReadLine().Trim());
+            BigInteger F = 0, F3, F4;
+
+            if (N != 2)
+            {
+                F3 = (k - 1) * (k * k - 1);
+                F4 = (k - 1) * (k * k - 1) * k - (k - 1) * (k - 1);
+                if (N == 3) F = F3;
+                else if (N == 4) F = F4;
+                else
+                {
+                    for (int i = 4; i < N; i++)
+                    {
+                        F = (k - 1) * (F3 + F4);
+                        F3 = F4;
+                        F4 = F;
+                    }
+                }
+            }
+
+            Console.WriteLine(F % M);
+            Console.ReadLine();
+        }
+    }
+}
