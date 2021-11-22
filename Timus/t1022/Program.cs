5e61debdbb2a58f462ca6accf5a8022c87a9c549
lesha13950 <kurepin1985@mail.ru>
2021-11-22 14:10:53 +0400

    Tasl 1022 solution

--- Timus/t1022/Program.cs
+++ Timus/t1022/Program.cs
@@ -8,17 +8,26 @@ namespace t1022
 {
     class Program
     {
+        const int MAXN = 2;
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine().Trim());
-            string[] parentsOf = new string[N + 1];
-            int[] numOfParents = new int[N + 1];
+            string[] parentsOf = new string[MAXN + 1];
+            int[] numOfParents = new int[MAXN + 1];
+            string result = "";
+            int t = N;
+            int i, j;
 
-            for (int i = 0; i < N; i++)
+            for (i = 1; i <= MAXN ; i++) {
+                parentsOf[i] = "";
+                numOfParents[i] = 0;
+            }
+
+            for (i = 0; i < N; i++)
             {
                 string[] childs = Console.ReadLine().Trim().Split(' ');
 
-                for (int j = 0; j < childs.Length - 1; j++)
+                for (j = 0; j < childs.Length - 1; j++)
                 {
                     int child = int.Parse(childs[j]);
                     parentsOf[child] += (i + 1).ToString() + " ";
@@ -26,6 +35,20 @@ namespace t1022
                 }
             }
 
+            while (t > 0)
+            {
+                i = 1;
+                while (numOfParents[i] != 0) i++;
+                result += i.ToString() + " ";
+                numOfParents[i] = -1;
+                for (j = 1; j<= N; j++)
+                {
+                    if (parentsOf[j].IndexOf(i.ToString() + " ") >= 0) numOfParents[j]--;
+                }
+                t--;
+            }
+
+            Console.WriteLine(result.Trim());
             Console.ReadLine();
         }
     }
