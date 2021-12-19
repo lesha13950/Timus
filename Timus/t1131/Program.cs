f4172ebef12fdcec2465e75c4c48aed25e4eaa9a
lesha13950 <kurepin1985@mail.ru>
2021-12-19 18:37:24 +0400

    Task 1131 solution

---
+++ Timus/t1131/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Trim().Split(' ');
+            int N = int.Parse(tokens[0]), K = int.Parse(tokens[1]);
+            int time = 0, count = 1, wires = 1;
+
+            while (count < N)
+            {
+                if (wires == K)
+                {
+                    time += (N - count) / wires;
+                    if ((N - count) % wires > 0) time++;
+                    break;
+                }
+                count += wires;
+                time++;
+                wires *= 2;
+                if (wires > K) wires = K;
+            }
+
+            Console.WriteLine(time);
+            Console.ReadLine();
+        }
+    }
+}
