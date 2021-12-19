2d3b427de4e22d4f478f87efc2d61bef05d3ac32
lesha13950 <kurepin1985@mail.ru>
2021-12-19 15:37:49 +0400

    Task 1100 solution
    Not AC on Timus ("memory limit exceeded")

---
+++ Timus/t1100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            uint[] x = new uint[N];
+            int i, j;
+            for (i = 0; i < N; i++)
+            {
+                string[] tokens = Console.ReadLine().Trim().Split(' ');
+                x[i] = (uint.Parse(tokens[0]) << 8) + uint.Parse(tokens[1]);
+            }
+
+            for (i = 100; i >= 0; i--)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    if ((x[j] & 0xFF) == i) Console.WriteLine("{0} {1}", x[j] >> 8, i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
