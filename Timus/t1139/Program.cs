e5d5569a0c41311ddf9af0cb8bc29db41f43dba5
lesha13950 <kurepin1985@mail.ru>
2021-12-20 15:38:19 +0400

    Task 1139 solution

---
+++ Timus/t1139/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1139
+{
+    class Program
+    {
+        public const double e = 0.000001;
+        static int ytc(double y1, double y2)
+        {
+            int c = 0;
+            int A = (int)(y1 + e) + 1, B = (int)(y2 - e);
+            if (B >= A) c = B - A + 1;
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Trim().Split(' ');
+            int X = int.Parse(tokens[0]) - 1, Y = int.Parse(tokens[1]) - 1;
+            double k = (double)Y / X;
+            int c = 0;
+            
+            for (int i = 1; i <= X; i++)
+            {
+                c += 1 + ytc(k * (i - 1), k * i);
+            }
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
