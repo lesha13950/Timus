2f72f48cb4c5b745731303373669266f40090f0b
lesha13950 <kurepin1985@mail.ru>
2021-12-21 18:20:30 +0400

    Task 1194 solution

---
+++ Timus/t1194/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            int N = int.Parse(t[0]), K = int.Parse(t[1]);
+            Console.In.ReadToEnd();
+
+            Console.WriteLine((N - 1) * N / 2 - K);
+            Console.ReadLine();
+        }
+    }
+}
