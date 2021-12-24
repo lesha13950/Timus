e09e35aa0116ec0097cad134793cd0b6edaa77d8
lesha13950 <kurepin1985@mail.ru>
2021-12-24 19:05:09 +0400

    Task 1243 solution
    Not using BigInteger

---
+++ Timus/t1243/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1243
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine().Trim();
+            int res = 0;
+            int m = 1;
+
+            for (int i = N.Length - 1; i >= 0; i--)
+            {
+                int d = N[i] - '0';
+                res += m * d;
+                m *= 3;
+                m %= 7;
+                res %= 7;
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
