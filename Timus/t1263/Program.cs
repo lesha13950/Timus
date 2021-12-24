4220d69713d5e89356b01864c6b0d0a86c8203c6
lesha13950 <kurepin1985@mail.ru>
2021-12-24 18:55:17 +0400

    Task 1263 solution

---
+++ Timus/t1263/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1263
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            int N = int.Parse(t[0]), M = int.Parse(t[1]);
+            int[] candidates = new int[N];
+            int i;
+
+            for (i = 0; i < M; i++)
+            {
+                int c = int.Parse(Console.ReadLine().Trim());
+                candidates[c - 1]++;
+            }
+
+            for (i = 0; i < N; i++)
+            {
+                Console.WriteLine("{0:f2}%", 100.0 * candidates[i] / M);
+            }
+            Console.ReadLine();
+        }
+    }
+}
