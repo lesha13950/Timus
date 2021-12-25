fb972eb992ce74985db93ab3b1c2baffdd37eb71
lesha13950 <kurepin1985@mail.ru>
2021-12-25 16:48:13 +0400

    Task 1224 solution

---
+++ Timus/t1224/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            ulong N = ulong.Parse(t[0]), M = ulong.Parse(t[1]);
+            ulong turns;
+
+            if (N <= M) turns = 2 * N - 2;
+            else turns = 2 * M - 1;
+
+            Console.WriteLine(turns);
+            Console.ReadLine();
+        }
+    }
+}
