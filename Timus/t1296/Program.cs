f446b716e2e8b8d2504725422b3e9dfa24060e00
lesha13950 <kurepin1985@mail.ru>
2021-12-26 11:46:23 +0400

    Task 1296 solution
    Search max sub-array again (1d case)

--- Timus/t1296/Program.cs
+++ Timus/t1296/Program.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace t1290
+namespace t1296
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int sum = 0, max = 0;
             int N = int.Parse(Console.ReadLine().Trim());
             int[] A = new int[N];
             int i;
             for (i = 0; i < N; i++) A[i] = int.Parse(Console.ReadLine().Trim());
-            Array.Sort(A);
-            Array.Reverse(A);
-            for (i = 0; i < N; i++) Console.WriteLine(A[i]);
+
+            for (i = 0; i < N; i++)
+            {
+                sum = Math.Max(sum + A[i], 0);
+                max = Math.Max(max, sum);
+            }
+
+            Console.WriteLine(max);
             Console.ReadLine();
         }
     }
