2fdc02a57dac6972d7668a317a296b914e11a02c
lesha13950 <kurepin1985@mail.ru>
2021-12-29 21:22:42 +0400

    Task 1370 solution

---
+++ Timus/t1370/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1370
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            int N = int.Parse(t[0]), M = int.Parse(t[1]);
+            int[] A = new int[N];
+            int i;
+            for (i = 0; i < N; i++) A[i] = int.Parse(Console.ReadLine().Trim());
+            int start = M % N;
+            for (i = 0; i < 10; i++) Console.Write(A[(start + i) % N]);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
