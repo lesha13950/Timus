974ac96c8fbf02dc5c9f79ad95748051cd917bc9
lesha13950 <kurepin1985@mail.ru>
2021-12-29 21:58:19 +0400

    Task 1581 solution
    Remark: Incorrect task statement

---
+++ Timus/t1581/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1581
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            int n, c;
+            int i;
+            string[] t = Console.ReadLine().Trim().Split(' ');
+
+            i = 0;
+            while (i < N)
+            {
+                if (i != 0) Console.Write(' ');
+                n = int.Parse(t[i]);
+                c = 0;
+                while (i < N && int.Parse(t[i]) == n)
+                {
+                    c++;
+                    i++;
+                }
+                Console.Write("{0} {1}", c, n);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
