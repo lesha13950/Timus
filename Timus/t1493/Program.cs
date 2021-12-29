02361c31be1454a9f2ddc1475756ca34a4c971dd
lesha13950 <kurepin1985@mail.ru>
2021-12-29 21:40:32 +0400

    Task 1493 solution

---
+++ Timus/t1493/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1493
+{
+    class Program
+    {
+        static bool isLucky(string t)
+        {
+            return (t[0] + t[1] + t[2]) == (t[3] + t[4] + t[5]);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            string next = "", prev = "";
+
+            next = (n + 1).ToString("d6");
+            prev = (n - 1).ToString("d6");
+            if (isLucky(next) || isLucky(prev)) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+            Console.ReadLine();
+        }
+    }
+}
