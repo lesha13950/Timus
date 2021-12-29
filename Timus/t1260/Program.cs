311fdc5561ff244a995278f0b2e64ce3d06fac0b
lesha13950 <kurepin1985@mail.ru>
2021-12-29 21:00:19 +0400

    Task 1260 solution

---
+++ Timus/t1260/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1260
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            int[] F = new int[56];
+            F[0] = 0;
+            F[1] = 1;
+            F[2] = 1;
+            for (int i = 3; i <= N; i++) F[i] = F[i - 1] + F[i - 3] + 1;
+
+            Console.WriteLine(F[N]);
+            Console.ReadLine();
+        }
+    }
+}
