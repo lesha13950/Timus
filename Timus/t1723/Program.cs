7317845617be4e05c7e9a5aaae7d040c6fb19505
lesha13950 <kurepin1985@mail.ru>
2021-12-31 00:36:09 +0400

    Task 1723 solution

---
+++ Timus/t1723/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1723
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Trim();
+            int[] c = new int[256];
+            int indmax = 0;
+            int i;
+
+            for (i = 0; i < s.Length; i++) c[s[i]]++;
+            for (i = 1; i < 256; i++)
+            {
+                if (c[i] > c[indmax]) indmax = i;
+            }
+            Console.WriteLine((char)indmax);
+            Console.ReadLine();
+        }
+    }
+}
