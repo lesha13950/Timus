7ca5d6bb16fac22a44529cff364539250bc9a2c5
lesha13950 <kurepin1985@mail.ru>
2021-12-30 17:17:22 +0400

    Task 1607 solution

---
+++ Timus/t1607/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1607
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            int a = int.Parse(t[0]), b = int.Parse(t[1]), c = int.Parse(t[2]), d = int.Parse(t[3]);
+            int deal = 0;
+
+            if (a >= c) deal = a;
+
+            while (deal == 0)
+            {
+                if (a + b > c) deal = c; else a += b;
+                if (deal == 0)
+                {
+                    if (c - d < a) deal = a; else c -= d;
+                }
+            }
+
+            Console.WriteLine(deal);
+            Console.ReadLine();
+        }
+    }
+}
