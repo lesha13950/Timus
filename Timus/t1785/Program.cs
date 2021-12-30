98b211fa6f8f735879d8613285d25ae2d1b949be
lesha13950 <kurepin1985@mail.ru>
2021-12-31 00:08:09 +0400

    Task 1785 solution
    Accuracy test...

---
+++ Timus/t1785/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1785
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            if (n <= 4) Console.WriteLine("few");
+            else if (n <= 9) Console.WriteLine("several");
+            else if (n <= 19) Console.WriteLine("pack");
+            else if (n <= 49) Console.WriteLine("lots");
+            else if (n <= 99) Console.WriteLine("horde");
+            else if (n <= 249) Console.WriteLine("throng");
+            else if (n <= 499) Console.WriteLine("swarnm");
+            else if (n <= 999) Console.WriteLine("zounds");
+            else Console.WriteLine("legion");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Timus/t1785/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("t1785")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("t1785")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c4fa838b-2686-427c-93f8-7aa3d5173ebc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
