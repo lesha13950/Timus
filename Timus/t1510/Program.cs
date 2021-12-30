0164aeb495536531ad39ad4519a4f9b491238bd2
lesha13950 <kurepin1985@mail.ru>
2021-12-30 16:22:54 +0400

    Task 1510 solution
    Note! Without use Garbage Collector we're got MLE

---
+++ Timus/t1510/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1510
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            int nominal = -1;
+            int c = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int cur = int.Parse(Console.ReadLine().Trim());
+                if (c == 0)
+                {
+                    nominal = cur;
+                    c = 1;
+                }
+                else if (cur == nominal)
+                {
+                    c++;
+                }
+                else
+                {
+                    c--;
+                }
+            }
+            Console.WriteLine(nominal);
+            Console.ReadLine();
+        }
+    }
+}
