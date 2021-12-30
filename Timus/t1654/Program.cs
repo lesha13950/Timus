0135b819708ccefe1a0ec16a7c2cb6c020597d8a
lesha13950 <kurepin1985@mail.ru>
2021-12-30 23:45:09 +0400

    Task 1654 v2
    Using stack

--- Timus/t1654/Program.cs
+++ Timus/t1654/Program.cs
@@ -10,36 +10,19 @@ namespace t1654
     {
         static void Main(string[] args)
         {
-            List<char> s = new List<char>(Console.ReadLine().Trim().ToCharArray());
-            int i, j;
+            Stack<char> s = new Stack<char>();
+            string str = Console.ReadLine().Trim();
+            char[] c;
 
-            i = 0;
-            while (i < s.Count - 1)
+            for (int i = 0; i < str.Length; i++)
             {
-                if (s[i] == s[i + 1])
-                {
-                    j = 2;
-                    while (i + j < s.Count && s[i] == s[i + j]) j++;
-                    if (j % 2 == 1)
-                        s.RemoveRange(i, j - 1);
-                    else
-                        s.RemoveRange(i, j);
-
-                    if (j % 2 == 0)
-                    {
-                        while (i - 1 >= 0 && i < s.Count && s[i] == s[i - 1])
-                        {
-                            i--;
-                            s.RemoveRange(i, 2);
-                        }
-                    }
-                }
-                else
-                {
-                    i++;
-                }
+                if (s.Count > 0 && str[i] == s.Peek()) s.Pop();
+                else s.Push(str[i]);
             }
-            Console.WriteLine(s.ToArray());
+
+            c = s.ToArray();
+            Array.Reverse(c);
+            Console.WriteLine(c);
             Console.ReadLine();
         }
     }
