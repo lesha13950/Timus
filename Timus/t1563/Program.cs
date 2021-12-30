90288282cea593c0fc52451992116d13dd0a0646
lesha13950 <kurepin1985@mail.ru>
2021-12-30 15:52:10 +0400

    Task 1563 solution

---
+++ Timus/t1563/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1563
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            Dictionary<string, bool> buticks = new Dictionary<string, bool>();
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                buticks[s] = true;
+            }
+            Console.WriteLine(N - buticks.Count);
+            Console.ReadLine();
+        }
+    }
+}
