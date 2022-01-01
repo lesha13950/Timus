5e8ac0cb239e1f0354c3673d014dd8fc7d411140
lesha13950 <kurepin1985@mail.ru>
2022-01-01 16:51:12 +0400

    Task 1880 solution
    Naive solution

---
+++ Timus/t1880/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1880
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] a = new int[4000];
+            List<int> l = new List<int>();
+            string[] t;
+            int i, j;
+
+            n = int.Parse(Console.ReadLine().Trim());
+            t = Console.ReadLine().Trim().Split(' ');
+            for (i = 0; i < n; i++) l.Add(int.Parse(t[i]));
+
+            for (i = 1; i < 3; i++)
+            {
+                Array.Clear(a, 0, 4000);
+                n = int.Parse(Console.ReadLine().Trim());
+                t = Console.ReadLine().Trim().Split(' ');
+                for (j = 0; j < n; j++) a[j] = int.Parse(t[j]);
+                j = 0;
+                while (j < l.Count)
+                {
+                    if (!a.Contains(l[j])) l.RemoveAt(j);
+                    else j++;
+                }
+            }
+            Console.WriteLine(l.Count);
+            Console.ReadLine();
+        }
+    }
+}
