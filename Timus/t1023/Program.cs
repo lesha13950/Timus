13586c9a06e55de546ec1143f5a842042ec14404
lesha13950 <kurepin1985@mail.ru>
2022-01-01 19:41:03 +0400

    Task 1023 solution

---
+++ Timus/t1023/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine().Trim());
+            int L = 3;
+
+            while (K % L != 0) L++;
+            Console.WriteLine(L - 1);
+            Console.ReadLine();
+        }
+    }
+}
