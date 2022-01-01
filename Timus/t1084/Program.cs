b36e817f925319e08210ea0d0a4f2fe765ac9580
lesha13950 <kurepin1985@mail.ru>
2022-01-01 17:47:47 +0400

    Task 1084 solution

---
+++ Timus/t1084/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1084
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            double a = double.Parse(t[0]), l = double.Parse(t[1]);
+            double s = 0;
+
+            if (l <= a / 2)
+                s = Math.PI * l * l;
+            else if (l < a / Math.Sqrt(2))
+            {
+                double alpha = Math.PI / 2 - 2 * Math.Acos(a / (2 * l));
+                s = 2 * alpha * l * l + a * Math.Sqrt(4 * l * l - a * a);
+            }
+            else s = a * a;
+            Console.WriteLine("{0:f3}", s);
+            Console.ReadLine();
+        }
+    }
+}
