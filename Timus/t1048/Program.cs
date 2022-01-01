274a4042400bb7616130c618e01e19453117c850
lesha13950 <kurepin1985@mail.ru>
2022-01-01 18:27:03 +0400

    Task 1048 solution

---
+++ Timus/t1048/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine().Trim());
+            char[] a = new char[N], b = new char[N], s = new char[N];
+            int ta, tb, c = 0, d;
+            string t;
+            int i;
+            for (i = 0; i < N; i++)
+            {
+                if (i % 1000 == 0) GC.Collect();
+                t = Console.ReadLine().Trim();
+                a[i] = t[0];
+                b[i] = t[2];
+            }
+
+            for (i = N - 1; i >= 0; i--)
+            {
+                if (i % 1000 == 0) GC.Collect();
+                ta = a[i] - '0';
+                tb = b[i] - '0';
+                d = (ta + tb + c) % 10;
+                c = (ta + tb + c) / 10;
+                s[i] = (char)(d + '0');
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
