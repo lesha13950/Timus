c2a734cce6cffdc460ee02357de83db21998f909
lesha13950 <kurepin1985@mail.ru>
2022-01-01 18:39:43 +0400

    Task 1924 solution

---
+++ Timus/t1924/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1924
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            if (n % 4 == 1 || n % 4 == 2) Console.WriteLine("grimy");
+            else Console.WriteLine("black");
+            Console.ReadLine();
+        }
+    }
+}
