f38f705d7c7c4188b804df853934ea4bd71f493c
lesha13950 <kurepin1985@mail.ru>
2022-01-01 16:27:55 +0400

    Task 1820 solution

---
+++ Timus/t1820/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1820
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t = Console.ReadLine().Trim().Split(' ');
+            int n = int.Parse(t[0]), k = int.Parse(t[1]);
+            if (k >= n) Console.WriteLine("2");
+            else Console.WriteLine(Math.Ceiling(2.0 * n / k));
+            Console.ReadLine();
+        }
+    }
+}
