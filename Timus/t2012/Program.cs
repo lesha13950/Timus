7ddf4bed04321b540e0a07350165e728f590cdda
lesha13950 <kurepin1985@mail.ru>
2022-01-01 13:56:07 +0400

    Task 2012 solution

---
+++ Timus/t2012/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = int.Parse(Console.ReadLine().Trim());
+            Console.WriteLine((f >= 7)? "YES": "NO");
+            Console.ReadLine();
+        }
+    }
+}
