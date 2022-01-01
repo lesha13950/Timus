a3154d936fd968335270dac4bcb2aa8616788373
lesha13950 <kurepin1985@mail.ru>
2022-01-01 16:35:40 +0400

    Task 2001 solution

---
+++ Timus/t2001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] t;
+            int[] a = new int[3], b = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                t = Console.ReadLine().Trim().Split(' ');
+                a[i] = int.Parse(t[0]);
+                b[i] = int.Parse(t[1]);
+            }
+            Console.WriteLine("{0} {1}", a[0] - a[2], b[0] - b[1]);
+            Console.ReadLine();
+        }
+    }
+}
