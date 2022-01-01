23f4b617427239dd3c6484214af33042c6f3d20f
lesha13950 <kurepin1985@mail.ru>
2022-01-01 17:25:04 +0400

    Task 1026 solution

---
+++ Timus/t1026/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1026
+{
+    class Program
+    {
+        const int DBSIZE = 100000;
+        const int MAXQUERY = 5000;
+        static void Main(string[] args)
+        {
+            int N, K;
+            int i;
+            int[] db = new int[DBSIZE];
+            int[] q = new int[100];
+
+            // db initialization
+            for (i = 0; i < DBSIZE; i++) db[i] = MAXQUERY + 1;
+
+            N = int.Parse(Console.ReadLine().Trim());
+            for (i = 0; i < N; i++) db[i] = int.Parse(Console.ReadLine().Trim());
+            Array.Sort(db);
+            Console.ReadLine(); // skip ###
+            K = int.Parse(Console.ReadLine().Trim());
+            for (i = 0; i < K; i++) q[i] = int.Parse(Console.ReadLine().Trim());
+            for (i = 0; i < K; i++) Console.WriteLine(db[q[i] - 1]);
+            Console.ReadLine();
+        }
+    }
+}
