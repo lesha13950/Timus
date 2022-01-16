32cd30496e41ece90a75796b0943168561df801c
lesha13950 <kurepin1985@mail.ru>
2022-01-16 20:52:21 +0400

    Task 1032 solution

---
+++ Timus/t1032/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = new int[N], b = new int[N];
+            int sum = 0;
+            int i, j;
+
+            // b - array of partial sums
+            // initialization
+            b[0] = -1;
+            for (i = 1; i < N; i++) b[i] = -2;
+
+            // read input array
+            for (i = 0; i < N; i++) a[i] = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < N; i++) {
+                sum = (sum + a[i]) % N;
+                if (b[sum] == -2) b[sum] = i;
+                else
+                {
+                    Console.WriteLine(i - b[sum]);
+                    for (j = b[sum] + 1; j <= i; j++) Console.WriteLine(a[j]);
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
