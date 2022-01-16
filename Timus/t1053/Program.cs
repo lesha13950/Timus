bde14cfb94ba35d360f05af91b368ce071c2d01c
lesha13950 <kurepin1985@mail.ru>
2022-01-16 16:53:13 +0400

    Task 1053 solution

---
+++ Timus/t1053/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1053
+{
+    class Program
+    {
+        static int gcd(int a, int b)
+        {
+            if (b == 0) return a;
+            return gcd(b, a % b);
+        }
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int l, g, i;
+            g = int.Parse(Console.ReadLine());
+            for (i = 1; i < N; i++)
+            {
+                l = int.Parse(Console.ReadLine());
+                g = gcd(g, l);
+            }
+            Console.WriteLine(g);
+            Console.ReadLine();
+        }
+    }
+}
