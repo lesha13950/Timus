d22c1a4ed921b8c692be08d80b05a1d96a9ab3e9
lesha13950 <kurepin1985@mail.ru>
2022-02-07 18:17:10 +0400

    Task 1078 solution - v1
    Got TLE on test #5

---
+++ Timus/t1078/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1078
+{
+    class Program
+    {
+        static List<int>[] nesting;
+        static (int count, string sequence)[] ss; // ss = "segments sequences"
+
+        // True if segment "x" is nested in segment "y"
+        static bool IsNested(int x1, int x2, int y1, int y2)
+        {
+            return (y1 < x1 && x2 < y2);
+        }
+
+        static (int, string) GetLongestSequence(int segnum)
+        {
+            if (ss[segnum].count != 0) return ss[segnum];
+
+            (int count, string sequence) t1 = (0, ""), t2;
+            foreach (int num in nesting[segnum])
+            {
+                t2 = GetLongestSequence(num);
+                if (t2.count > t1.count) t1 = t2;
+            }
+
+            return (1 + t1.count, (segnum + 1).ToString() + " " + t1.sequence);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] t;
+            nesting = new List<int>[n];
+            (int, int)[] seg = new (int, int)[n];
+            ss = new (int count, string sequence)[n];
+            int i, j;
+            int longest = 0;
+
+            for (i = 0; i < n; i++)
+            {
+                t = Console.ReadLine().Split(' ');
+                seg[i] = (int.Parse(t[0]), int.Parse(t[1]));
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                nesting[i] = new List<int>();
+                for (j = 0; j < n; j++)
+                {
+                    if (IsNested(seg[i].Item1, seg[i].Item2, seg[j].Item1, seg[j].Item2)) nesting[i].Add(j);
+                }
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                if (ss[i].count == 0) ss[i] = GetLongestSequence(i);
+            }
+
+            for (i = 1; i < n; i++)
+            {
+                if (ss[i].count > ss[longest].count) longest = i;
+            }
+            Console.WriteLine(ss[longest].count);
+            Console.WriteLine(ss[longest].sequence.Trim());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Timus/t1078/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("t1078")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("t1078")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d7be2c4e-1a1d-4090-8818-864128c9625a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
